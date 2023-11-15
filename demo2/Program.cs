765c64ff9b59148c131e74aab989b07e927edcdd
Kulbhushan <karnkulbhushan@gmail.com>
2023-11-15 11:31:46 +0530

    Add project files.

---
+++ demo2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+class Person
+{
+    
+    public string Name { get; set; }
+    public int Age { get; set; }
+
+    
+    public Person()
+    {
+        
+        Name = "XYZ";
+        Age = 30;
+    }
+
+    
+    public void DisplayInfo()
+    {
+        Console.WriteLine($"Name: {Name}, Age: {Age}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        
+        Person person = new Person();
+
+        
+        Console.WriteLine("Default Attributes:");
+        person.DisplayInfo();
+
+       
+        person.Name = "ABCD";
+        person.Age = 25;
+
+        
+        Console.WriteLine("\nModified Attributes:");
+        person.DisplayInfo();
+    }
+}
+
